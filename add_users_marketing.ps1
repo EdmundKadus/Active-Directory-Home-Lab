d9240e4142446c7b05bf64e567194b7253e3dd93
Edmund <edomundoo@gmail.com>
2025-01-20 04:47:10 +0800

    Added a txt file & script
    Added a randomly generated user list (With chatgpt) and a script I modified to add users to existing OU

---
+++ add_users_marketing.ps1
@@ -0,0 +1,32 @@
+﻿#Create a variable for password that all users in the list would use#
+$USERS_PASSWORD = "P@ssw0rd1"
+#Pulls randomly generated names by chatgpt on a text file, there are about 50 names in it#
+$USERS_NAMELIST = Get-Content .\user_list.txt
+
+
+
+$password = ConvertTo-SecureString $USERS_PASSWORD -AsPlainText -Force
+#Insert these users into my exist OU named Marketing#
+Set-ADOrganizationalUnit -Identity "ou=Marketing,DC=edmundlabs,DC=com" -ProtectedFromAccidentalDeletion $false
+
+
+#foreach loop
+
+foreach ($n in $USERS_NAMELIST){
+    
+    $first = $n.Split("")[0].ToLower()
+    $last = $n.Split("")[0].ToLower()
+    #Append to create username, substring index the first letter of $first name#
+    $username = "$($first.substring(0,1))$($last)".ToLower()
+    Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Red
+
+           New-AdUser -AccountPassword $password `
+               -GivenName $first `
+               -Surname $last `
+               -DisplayName $username `
+               -Name $username `
+               -EmployeeID $username `
+               -PasswordNeverExpires $true `
+               -Path "ou=_Marketing,$(([ADSI]`"").distinguishedName)" `
+               -Enabled $true
+}
