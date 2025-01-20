4ed7e218c2783561e6c924f1047326cef8d95cf4
Edmund <edomundoo@gmail.com>
2025-01-20 11:24:17 +0800

    Modified add_users_marketing.ps1
    Did some change to the script for it to be added to existing OUs
    
    -Specify path
    -Write-Host

--- add_users_marketing.ps1
+++ add_users_marketing.ps1
@@ -1,32 +1,24 @@
-﻿#Create a variable for password that all users in the list would use#
-$USERS_PASSWORD = "P@ssw0rd1"
-#Pulls randomly generated names by chatgpt on a text file, there are about 50 names in it#
+$USERS_PASSWORD = "Password1"
 $USERS_NAMELIST = Get-Content .\user_list.txt
-
-
-
 $password = ConvertTo-SecureString $USERS_PASSWORD -AsPlainText -Force
-#Insert these users into my exist OU named Marketing#
-Set-ADOrganizationalUnit -Identity "ou=Marketing,DC=edmundlabs,DC=com" -ProtectedFromAccidentalDeletion $false
 
-
-#foreach loop
+# Specify the target OU explicitly
+$targetOU = "OU=Marketing,DC=edmundlabs,DC=com" 
 
 foreach ($n in $USERS_NAMELIST){
-    
-    $first = $n.Split("")[0].ToLower()
-    $last = $n.Split("")[0].ToLower()
-    #Append to create username, substring index the first letter of $first name#
-    $username = "$($first.substring(0,1))$($last)".ToLower()
+    $first = $n.Split(" ")[0].ToLower()
+    $last = $n.Split(" ")[1].ToLower()
+    $username = "$($first.Substring(0,1))$($last)".ToLower()
     Write-Host "Creating user: $($username)" -BackgroundColor Black -ForegroundColor Red
 
-           New-AdUser -AccountPassword $password `
+    # Add the -Path parameter to specify the target OU
+    New-AdUser -AccountPassword $password `
                -GivenName $first `
                -Surname $last `
                -DisplayName $username `
                -Name $username `
                -EmployeeID $username `
                -PasswordNeverExpires $true `
-               -Path "ou=_Marketing,$(([ADSI]`"").distinguishedName)" `
-               -Enabled $true
+               -Enabled $true `
+               -Path $targetOU 
 }
